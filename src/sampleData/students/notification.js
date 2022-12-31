b30e7a3e4a7b23305fddbafb27e47cd684721118
Jonslogar Arenque <jonwiths@gmail.com>
2022-12-31 15:40:28 +0800

    added student-dashboard test3

---
+++ src/sampleData/students/notification.js
@@ -0,0 +1,40 @@
+import logo from '../../assets/logos/maven-logo-2.jpg';
+const notification = [
+  {
+    id: 1,
+    name: 'Jon Doe',
+    message: 'has message to you.',
+    time: 'Just now',
+    img_url: logo
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    message: 'has message to you.',
+    time: 'a minute ago',
+    img_url: logo
+  },
+  {
+    id: 3,
+    name: 'Will Smith',
+    message: 'Added profile pic.',
+    time: '10:00 AM',
+    img_url: logo
+  },
+  {
+    id: 4,
+    name: 'Karen',
+    message: 'has message to you.',
+    time: '12:00 PM',
+    img_url: logo
+  },
+  {
+    id: 5,
+    name: 'Karen XD',
+    message: 'has message to you.',
+    time: '12:00 PM',
+    img_url: logo
+  }
+];
+
+export default notification;
